786f2440e75c55aa4e2186efdfe4c86f39640f81
Prince Raj Rawat <prince1234rawat4321@gmail.com>
2025-08-22 22:44:33 +0530

    Add q1.c and q2.c

---
+++ day1/q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Enter a number: ");
+    scanf("%d", &a); 
+    printf("Enter another number: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
---
+++ day1/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a, b, product,sum, difference, quotient;
+    printf("Enter a number: ");
+    scanf("%d", &a); 
+    printf("Enter another number: ");
+    scanf("%d", &b);
+    product = a * b;
+    printf("The product of %d and %d is %d\n", a, b, product);
+        difference = a - b; 
+    printf("The difference of %d and %d is %d\n", a, b, difference);
+    quotient = a / b;
+    printf("The quotient of %d and %d is %d\n", a, b, quotient);
+    sum = a + b;
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
