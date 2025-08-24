067d500efc726c1ef439cbfc6589a1656887583e
Prince Raj Rawat <prince1234rawat4321@gmail.com>
2025-08-25 00:29:14 +0530

    removed all day folders

---
+++ q7.c
@@ -0,0 +1,15 @@
+//Q7: Write a program to swap two numbers without using a third variable.
+
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("enter another ")
+
+    
+    return 0;
+}
