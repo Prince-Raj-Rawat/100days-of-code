ecedd9660a6e93b1135f191d6c48a58bb28eccf3
Prince Raj Rawat <prince1234rawat4321@gmail.com>
2025-08-26 22:18:32 +0530

    added q11 and q12

--- day5/q10.c
+++ day5/q10.c
@@ -1,3 +1,5 @@
+//Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
 
 
 #include <stdio.h>
@@ -8,12 +10,7 @@ scanf("%d",&sec);
 m=(sec%3600)/60;
 h=(sec/3600);
 s=(sec%3600)%60;
-printf("%d",m);
 printf("the time is %d:%d:%d",h,m,s);
 return 0;
   
-
-
-
-
 }
